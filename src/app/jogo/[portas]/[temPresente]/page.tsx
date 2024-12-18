02cd0e458f4ba5331c0857439ddb505183c0fe6b
Victor <victorlima0302@gmail.com>
2024-12-18 15:11:02 -0300

    feat: adiciona a lógica de puxar as váriaveis pela URL

---
+++ src/app/jogo/[portas]/[temPresente]/page.tsx
@@ -0,0 +1,47 @@
+"use client"; // Asegura que o componente será renderizado no cliente
+
+import styles from "../../../../styles/jogo.module.css";
+import { useState, useEffect } from "react";
+import Porta from "../../../../components/Porta";
+import PortaModel from "../../../../model/porta";
+import { atualizarPortas, criarPortas } from "../../../../functions/portas";
+import Link from "next/link";
+import { useParams } from "next/navigation";
+
+export default function Jogo() {
+    const { portas, temPresente } = useParams();
+    const [portasState, setPortas] = useState([]);
+
+    useEffect(() => {
+        if (portas && temPresente) {
+            const portasNum = +portas;
+            const temPresenteNum = +temPresente;
+            setPortas(criarPortas(portasNum, temPresenteNum));
+        }
+    }, [portas, temPresente]);
+
+    function renderizarPortas() {
+        return portasState.map((porta) => {
+            return (
+                <Porta
+                    key={porta.numero}
+                    value={porta}
+                    onChange={(novaPorta: PortaModel) =>
+                        setPortas(atualizarPortas(portasState, novaPorta))
+                    }
+                />
+            );
+        });
+    }
+
+    return (
+        <div id={styles.jogo}>
+            <div className={styles.portas}>{renderizarPortas()}</div>
+            <div className={styles.botoes}>
+                <Link href="/">
+                    <button>Reiniciar Jogo</button>
+                </Link>
+            </div>
+        </div>
+    );
+}
--- src/components/Porta.tsx
+++ src/components/Porta.tsx
@@ -1,3 +1,4 @@
+"use client";
 import styles from "../styles/porta.module.css";
 import Presente from "./Presente"
 import PortaModel from "../model/porta";
